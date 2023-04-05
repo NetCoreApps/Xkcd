9ccd2a6958a5b64ca1dfcedd8cce55cbae0b9d35
Demis Bellot <demis.bellot@gmail.com>
2023-04-05 19:11:57 +0800

    move url to top of image

--- Xkcd/wwwroot/mjs/components/Comics.mjs
+++ Xkcd/wwwroot/mjs/components/Comics.mjs
@@ -9,6 +9,9 @@ export const ModalComic = {
             <h2 class="text-center my-8 font-display text-5xl font-bold tracking-tight text-slate-800 dark:text-slate-200">{{ comic.title }}</h2>
             <div class="px-8 flex justify-center">
                 <div class="pr-8">
+                    <p v-if="comic.url" class="text-center mb-2">
+                        <TextLink class="text-sm" :href="comic.url" target="_blank">{{comic.url}}</TextLink>
+                    </p>
                     <a :href="comic.url" :title="comic.url" target="_blank" class="block cursor-pointer hover:shadow-lg hover:bg-slate-50 dark:hover:bg-slate-900">
                         <img :src="comic.imageUrl" :width="comic.width" :height="comic.height" :aria-description="comic.explanation" :alt="comic.transcript" />
                     </a>
@@ -18,9 +21,6 @@ export const ModalComic = {
                     <h3 class="text-lg font-semibold font-mono">Transcript:</h3>
                     <p v-if="comic.transcript" class="pr-4 mb-2 sm:max-w-prose text-sm text-gray-600 dark:text-gray-300 font-mono text-xs">
                         {{comic.transcript}}
-                    </p>
-                    <p v-if="comic.url">
-                        <TextLink class="text-sm" :href="comic.url" target="_blank">{{comic.url}}</TextLink>
                     </p>
                 </div>
             </div>
